50d92390ca06194f49851f2296b513e28c6ba04d
Michał Klepner <michal.klepner1@o2.pl>
2025-03-27 11:18:59 +0100

    Added redux (store.js for sharing slices and separated logic task-slice so there is a place for readable adding new slices)

--- src/store/task-slice.js
+++ src/store/task-slice.js
@@ -11,5 +11,18 @@ const tasksSlice = createSlice({
     addTask(state, action) {
       state.tasks.push(action.payload);
     },
+    updateTaskStatus(state, action) {
+      const { id, status } = action.payload;
+      const task = state.tasks.find((task) => task.id === id);
+      if (task) {
+        task.status = status;
+      }
+    },
+    removeTask(state,action){
+      state.tasks = state.tasks.filter((task)=>task.id !== action.payload)
+    }
   },
 });
+
+export const tasksActions = tasksSlice.actions;
+export default tasksSlice;
